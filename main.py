2a2a164e1be091f831ecd6c7cc083555f8b4759b
Alessandro Mamusa <alesardo@develer.com>
2022-06-17 22:10:09 +0200

    fix init and load

--- main.py
+++ main.py
@@ -181,15 +181,13 @@ class CityScene(Scene):
 
 class App:
     def __init__(self):
-        pyxel.init(width=128, height=128, caption="Albert's journey", fps=60)
-        pyxel.load("assets/albert.pyxres")
+        pyxel.init(width=128, height=128, title="Albert's voyage", fps=60, quit_key=pyxel.KEY_Q)
+        pyxel.load("assets/albert.pyxres", image=True, tilemap=True)
         self.city = CityScene(0, 0, 0, 0, 0, 16, 16)
 
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        if pyxel.btnp(pyxel.KEY_Q):
-            pyxel.quit()
         self.city.update()
 
     def draw(self):
