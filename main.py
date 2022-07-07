f30b207bb7aacd608e25898e8d6ed48b79023685
Alessandro Mamusa <alesardo@develer.com>
2022-07-07 23:54:37 +0200

    scaffold game state to manage scenes

--- main.py
+++ main.py
@@ -3,7 +3,34 @@
 
 import pyxel
 
-from scenes import CityScene
+from scenes import SCENES, VICTORY_SCENE, GAME_OVER_SCENE
+
+
+class Game:
+    def __init__(self):
+        self._current_scene = 0
+        self.scene = SCENES[self._current_scene]
+
+    def start(self):
+        self._scene_instance = self.scene(0, 0, 0, 0, 0, pyxel.width, pyxel.height, self)
+
+    def victory(self):
+        self.scene = VICTORY_SCENE
+
+    def gameOver(self):
+        self.scene = GAME_OVER_SCENE
+
+    def nextScene(self):
+        self._current_scene += 1
+        self.scene = SCENES[self._current_scene]
+        # destroy last scene
+        # init new scene
+
+    def update(self):
+        self._scene_instance.update()
+
+    def draw(self):
+        self._scene_instance.draw()
 
 
 class App:
@@ -12,15 +39,16 @@ class App:
             width=128, height=128, title="Albert's voyage", fps=60, quit_key=pyxel.KEY_Q
         )
         pyxel.load("assets/albert.pyxres", image=True, tilemap=True)
-        self.city = CityScene(0, 0, 0, 0, 0, pyxel.width, pyxel.height)
-
+        # self.city = CityScene(0, 0, 0, 0, 0, pyxel.width, pyxel.height)
+        self.game = Game()
+        self.game.start()
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.city.update()
+        self.game.update()
 
     def draw(self):
-        self.city.draw()
+        self.game.draw()
 
 
 App()
--- scenes.py
+++ scenes.py
@@ -8,7 +8,7 @@ S_T_M = 8
 
 
 class Scene:
-    def __init__(self, x, y, tm, u, v, w, h):
+    def __init__(self, x, y, tm, u, v, w, h, game):
         self.x = x
         self.y = y
         self.tm = tm
@@ -17,6 +17,7 @@ class Scene:
         self.v = v
         self.w = w
         self.h = h
+        self.game = game
 
     def update(self):
         pass
@@ -26,8 +27,8 @@ class Scene:
 
 
 class CityScene(Scene):
-    def __init__(self, x, y, tm, u, v, w, h):
-        super().__init__(x, y, tm, u, v, w, h)
+    def __init__(self, x, y, tm, u, v, w, h, game):
+        super().__init__(x, y, tm, u, v, w, h, game)
         self.characters = []
         self.enemies = []
         player = Player(x + 1 * S_T_M, y + 6 * S_T_M, tm)
@@ -35,20 +36,31 @@ class CityScene(Scene):
         self.characters.append(player)
         self.enemies.append(enemy)
 
+        self.turn = 0
+
     def update(self):
-        for c in self.characters:
-            c.update()
-            if not c.p._is_shoot:
-                continue
+        for character in self.characters:
+            character.update()
+            # if not character.projectile._is_shoot:
+            #     continue
             for enemy in self.enemies:
                 if (
-                    enemy.x + enemy.w > c.p.x
-                    and c.p.x > enemy.x
-                    and enemy.y + enemy.h > c.p.y
-                    and c.p.y > enemy.y
+                    # check for enemies hitten
+                    enemy.x + enemy.w > character.projectile.x
+                    and character.projectile.x > enemy.x
+                    and enemy.y + enemy.h > character.projectile.y
+                    and character.projectile.y > enemy.y
                 ):
-                    c.p._has_hit = True
-                    c.p._is_shoot = False
+                    # play enemy death animation
+                    character.projectile._has_hit = True
+                    self.enemies.pop()
+                    if len(self.enemies) == 0:
+                        self.game.victory()
+            if (character.projectile._has_hit):  # projectile hit something else
+                character.projectile._has_hit = True
+                character.projectile._is_shoot = False
+                character.endTurn()
+                self.turn += 1
 
         for e in self.enemies:
             e.update()
@@ -59,3 +71,8 @@ class CityScene(Scene):
             c.draw()
         for e in self.enemies:
             e.draw()
+
+
+SCENES = [CityScene]
+VICTORY_SCENE = Scene
+GAME_OVER_SCENE = Scene
